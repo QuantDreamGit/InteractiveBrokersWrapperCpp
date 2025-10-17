bd45137279637d526c7f0f598545da0bc5062c86
QuantDreamGit <s283085@studenti.polito.it>
2025-10-17 08:08:31 +0200

    modified test

--- source/test.cpp
+++ source/test.cpp
@@ -1,16 +1,3 @@
 //
 // Created by Giuseppe Priolo on 15/10/25.
 //
-
-#include "../include/ibwrapper_test/test.h"
-
-#include <iostream>
-
-IBWrapper::IBWrapper() {
-  std::cout << "IBWrapper initialized." << std::endl;
-}
-
-void IBWrapper::connect(const std::string& host, int port, int clientId) {
-  std::cout << "Connecting to " << host << ":" << port
-            << " with clientId " << clientId << std::endl;
-}
